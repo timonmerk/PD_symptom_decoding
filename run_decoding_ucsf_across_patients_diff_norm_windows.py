39caf7fdc2f4340f9fd5d1810f3ac960c3f0c688
timonmerk <timon.merk95@gmail.com>
2025-02-18 12:01:57 +0100

    fix bool

--- run_decoding_ucsf_across_patients_diff_norm_windows.py
+++ run_decoding_ucsf_across_patients_diff_norm_windows.py
@@ -22,7 +22,7 @@ if __name__ == "__main__":
     PLT_PDF = False
 
     run_idx = int(sys.argv[1])
-    WITH_HOUR = bool(int(sys.argv[2]))
+    WITH_HOUR = bool(sys.argv[2])
     print(run_idx, WITH_HOUR)
     # run_idx is an integer running for each combination of label_name and norm_window
     label_names = ["pkg_bk", "pkg_dk", "pkg_tremor"]
