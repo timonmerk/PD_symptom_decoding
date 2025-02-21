24e3606e1c72d94d3eb098fef378d70f10d6fef3
timonmerk <timon.merk95@gmail.com>
2025-02-21 08:28:36 +0100

    fix cpu

--- run_dur.sh
+++ run_dur.sh
@@ -2,7 +2,7 @@
 #SBATCH --mem=100GB
 #SBATCH --partition=medium
 #SBATCH --time=23:00:00
-#SBATCH --constraint=skylake
+#SBATCH --constraint=haswell|broadwell|skylake
 #SBATCH -o logs/dur.out
 #SBATCH -e logs/dur.err
 #SBATCH -a 0
