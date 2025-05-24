54b98682ed3cfaca0a30e363f405d6bb06eacb31
timonmerk <timon.merk95@gmail.com>
2025-05-24 10:33:31 -0500

    add nighttime

--- run_nighttime.sh
+++ run_nighttime.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 #SBATCH --mem=100GB
-#SBATCH --partition=short
-#SBATCH --time=04:00:00
+#SBATCH --partition=medium
+#SBATCH --time=7-00:00:00
 #SBATCH -o logs/nighttime.out
 #SBATCH -e logs/nighttime.err
-#SBATCH -a 0-11
+#SBATCH -a 11
 
 uv run run_decoding_ucsf_across_patients_nighttime.py $SLURM_ARRAY_TASK_ID
